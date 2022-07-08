6924283db5ae13e7ca982edc304a70d3f250c44f
Jonathan Manchego Sosa <jonathan571999@gmail.com>
2022-07-08 00:11:36 -0500

    despliegue de prueba con health-check

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const databaseTest = require("./src/server/database");
-const router = require("./src/interfaces/routes");
+const router = require("./src/infraestructura/interfaces/routes");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
--- src/infraestructura/interfaces/routes/index.js
+++ src/infraestructura/interfaces/routes/index.js
@@ -1,6 +1,9 @@
 const apiRoutes = require("./api");
-module.exports = (app) => {
+module.exports = app => {
   app.use("/api", apiRoutes);
+  app.get("/health-check", (request, response) => {
+    response.json({ status: "OK" });
+  });
   app.get("/", (request, response) => {
     response.send("Hola desde el backend");
   });
